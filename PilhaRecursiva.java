98166659d5e18055b5e78a5516446d2630fd1f17
Julia Graziela Stein <julia161089@gmail.com>
2019-10-20 22:23:38 -0300

    implentando pilha com array

---
+++ PilhaRecursiva.java
@@ -0,0 +1,24 @@
+package pilha;
+
+public class PilhaRecursiva {
+
+	public int obterTamanho() {
+		return 0;
+
+	}
+
+	public void empilhar() {
+
+	}
+
+	public int desempilhar() {
+		return 0;
+
+	}
+
+	public int obterTopo() {
+		return 0;
+
+	}
+
+}
